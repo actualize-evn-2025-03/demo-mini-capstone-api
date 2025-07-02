ff53839c991bd085a02f700e08af2160c71d9184
Leon Shimizu <lmshimizu@gmail.com>
2025-07-02 12:09:02 +1000

    Fix up the associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :supplier
   has_many :images
-  has_many :orders
+  has_many :carted_products
+  has_many :orders, through: :carted_products
   has_many :category_products
   has_many :categories, through: :category_products
-
-  # def categories
-  #   category_products.map do |category_product|
-  #     category_product.category
-  #   end
-  # end
 end
