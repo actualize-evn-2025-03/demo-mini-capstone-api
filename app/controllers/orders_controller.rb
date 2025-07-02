88f95d906ca116ee415fa4bda8ee35f53147baac
Leon Shimizu <lmshimizu@gmail.com>
2025-07-02 12:23:07 +1000

    Update the orders create to properly work with the carted products

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,26 +10,32 @@ class OrdersController < ApplicationController
   end
 
   def create
-    product = Product.find(params[:product_id])
+    # grab the carted products for this user where the status is carted
+    carted_products = current_user.carted_products.where(status: "carted")
 
-    subtotal = product.price * params[:quantity].to_i
-    tax = subtotal * 0.08
-    total = tax + subtotal
+    # calculate the subtotal, tax, and total, based on the quantities from the carted product and the price of each product
+    calculated_subtotal = 0
 
-    @order = Order.new(
-      quantity: params[:quantity],
-      subtotal: subtotal,
-      tax: tax,
-      total: total,
+    carted_products.each do |carted_product|
+      calculated_subtotal += carted_product.quantity * carted_product.product.price
+    end
+
+    calculated_tax = calculated_subtotal * 0.08
+    calculated_total = calculated_tax + calculated_subtotal
+
+    # create the order with the calculations and the user's id
+    @order = Order.create(
       user_id: current_user.id,
-      product_id: params[:product_id]
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total
     )
 
-    if @order.save
-      render json: @order, status: :created
-    else
-      render json: { errors: @order.errors.full_messages }, status: :bad_request
-    end
+    # update all of the user's carted products with the status of carted to be "purchased"
+    carted_products.update_all(status: "purchased", order_id: @order.id)
+
+    # render the order
+    render json: @order
   end
 
   def show
